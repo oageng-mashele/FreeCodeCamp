32b3183c4de4f96c2a7ddc1db3019a357165ce11
omashele021 <omashele021@student.wethinkcode.co.za>
2023-02-01 21:28:22 +0200

    step 59 - 70

--- JavaScript Algorithms and Data Structures/Basic_JavaScript/basic.js
+++ JavaScript Algorithms and Data Structures/Basic_JavaScript/basic.js
@@ -271,3 +271,130 @@ console.log("After: " + JSON.stringify(testArr));
 function welcomeToBooleans() {
     return true;
 }
+
+//step59: IF statements
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue == true) {
+        return "Yes, that was true"
+    }
+    return "No, that was false"
+}
+
+//step60
+function testEqual(val) {
+    if (val == 12) { 
+      return "Equal";
+    }
+    return "Not Equal";
+}
+testEqual(10);
+
+//step61
+function testStrict(val) {
+    if (val===7) {
+      return "Equal";
+    }
+    return "Not Equal";
+}
+testStrict(10);
+
+//step62
+function compareEquality(a, b) {
+    if (a === b) { 
+      return "Equal";
+    }
+    return "Not Equal";
+}
+compareEquality(10, "10");
+
+//step63
+function testNotEqual(val) {
+    if (val != 99) { 
+      return "Not Equal";
+    }
+    return "Equal";
+}
+testNotEqual(10);
+
+//step64
+function testStrictNotEqual(val) {
+    if (val !== 17) {
+      return "Not Equal";
+    }
+    return "Equal";
+}
+testStrictNotEqual(10);
+
+//step65
+function testGreaterThan(val) {
+    if (val > 100) {  
+      return "Over 100";
+    }
+  
+    if (val > 10) { 
+      return "Over 10";
+    }
+  
+    return "10 or Under";
+}
+testGreaterThan(10);
+
+//step66
+function testGreaterOrEqual(val) {
+    if (val >= 20) {  
+      return "20 or Over";
+    }
+  
+    if (val >=10 ) { 
+      return "10 or Over";
+    }
+  
+    return "Less than 10";
+}
+testGreaterOrEqual(10);
+
+//step67
+function testLessThan(val) {
+    if (val < 25) {  
+      return "Under 25";
+    }
+  
+    if (val < 55) {  
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+}
+testLessThan(10);
+
+//step68
+function testLessOrEqual(val) {
+    if (val <= 12) {
+      return "Smaller Than or Equal to 12";
+    }
+  
+    if (val <= 24) {
+      return "Smaller Than or Equal to 24";
+    }
+  
+    return "More Than 24";
+}
+testLessOrEqual(10);
+
+//step69
+function testLogicalAnd(val) {
+    if (val <= 50 && val >=25) {
+        return "Yes";
+    }
+    return "No";
+}
+testLogicalAnd(10);
+
+//step70
+function testLogicalOr(val) {
+    if (val > 20 || val < 10) {
+      return "Outside";
+    }
+    return "Inside";
+}
+testLogicalOr(15);
