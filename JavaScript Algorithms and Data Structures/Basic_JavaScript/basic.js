f3255c31ad5c8b847dfe7f38bf082663de56cfb6
omashele021 <omashele021@student.wethinkcode.co.za>
2023-02-01 13:11:23 +0200

    step 1 -9

---
+++ JavaScript Algorithms and Data Structures/Basic_JavaScript/basic.js
@@ -0,0 +1,43 @@
+// step 1
+
+// This is an inline comment
+/* This is a multiline comment,
+ more stuff to write*/
+
+//step 2
+
+var myName;
+
+//step 3
+
+var a;
+a = 7;
+
+//step 4
+
+var b;
+b = a
+
+//step 5
+var c = 9;
+
+//step 6
+var myFirstName = "Oageng";
+var myLastName = "Mashele";
+
+//step 7
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+/* step 8:
+use camelCase when declaring variables */
+
+//step 9
+
+let catName = "Oli"
+let catSound = "Meow!"
