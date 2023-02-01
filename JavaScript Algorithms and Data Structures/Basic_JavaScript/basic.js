67bdac879c19d5c1100e675edebd4c369418df9a
omashele021 <omashele021@student.wethinkcode.co.za>
2023-02-01 13:44:23 +0200

    step 10 - 19

--- JavaScript Algorithms and Data Structures/Basic_JavaScript/basic.js
+++ JavaScript Algorithms and Data Structures/Basic_JavaScript/basic.js
@@ -38,6 +38,40 @@ c = c + " String!";
 use camelCase when declaring variables */
 
 //step 9
-
 let catName = "Oli"
 let catSound = "Meow!"
+
+//step10
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+fact = "is awesome"
+console.log(FCC, fact);
+
+//step11
+const sum = 10 + 10;
+
+//step12
+const difference = 45 - 33;
+
+//step13
+const product = 8 * 10;
+
+//step 14
+const quotient = 66 / 33;
+
+//step15
+let myVar = 87;
+myVar++;
+
+//step16
+myVar--;
+
+//step17
+const ourDecimal = 5.7;
+let myDecimal = 1.8;
+
+//step18
+const prod = 2.5 * 2.0;
+
+//step19
+const quot = 4.4 / 2.0;
