6dfeafacf8d3c69a58cb381377347ca79bd53675
Oageng Mashele <oageng.mashele@betterhome.co.za>
2024-08-19 19:01:50 +0200

    nested iteration and selection statements challenge

--- (New) Foundational C# with Microsoft/CsharpProjects/TestProject/Program.cs
+++ (New) Foundational C# with Microsoft/CsharpProjects/TestProject/Program.cs
@@ -1,29 +1,9 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
 
-// Your code goes here
-
-if (daysUntilExpiration <= 10) {
-    if (daysUntilExpiration <= 5) {
-
-        if (daysUntilExpiration == 1) {
-            Console.WriteLine("Your subscription expires within a day!\nRenew now and save 20%!");
-
-        }
-        else if (daysUntilExpiration == 0) {
-            Console.WriteLine("Your subscription has expired.");
-        }
-        else {
-             Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10%!");
-
-        }
-       
-    }   
-else {
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
 }
-    
-}
-
-
--- (New) Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ (New) Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a2fcfb890332e6a83c82039f416270a33c6be52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0ccf27dab0b7cd93df5027c7ebd29d38661206")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
