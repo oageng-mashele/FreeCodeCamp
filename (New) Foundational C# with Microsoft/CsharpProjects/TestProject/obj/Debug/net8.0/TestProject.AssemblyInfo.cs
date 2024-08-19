9f6974677aa8077e81028d61f3fa568ec4f08d6a
Oageng Mashele <oageng.mashele@betterhome.co.za>
2024-08-19 19:08:47 +0200

    improve code readability challenge

--- (New) Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ (New) Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0ccf27dab0b7cd93df5027c7ebd29d38661206")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dfeafacf8d3c69a58cb381377347ca79bd53675")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
