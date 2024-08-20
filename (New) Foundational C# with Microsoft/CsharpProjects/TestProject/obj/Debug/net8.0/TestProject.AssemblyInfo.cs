ed9b9ab00f703e3810287e51c24c10ccc10bcbbf
Oageng Mashele <oageng.mashele@betterhome.co.za>
2024-08-20 08:34:51 +0200

    Update your formatted output

--- (New) Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ (New) Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dfeafacf8d3c69a58cb381377347ca79bd53675")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37fc509e3fbe5a83a2fa2001e414d35ded3ce4af")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
