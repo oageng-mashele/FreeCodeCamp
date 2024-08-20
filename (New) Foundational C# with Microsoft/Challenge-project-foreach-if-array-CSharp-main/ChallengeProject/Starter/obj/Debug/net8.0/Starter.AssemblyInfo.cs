1aef8fd4b47e79343217aa8a4a3de9cc2108b2d9
Oageng Mashele <oageng.mashele@betterhome.co.za>
2024-08-20 08:38:28 +0200

    Update your calculated values

--- (New) Foundational C# with Microsoft/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ (New) Foundational C# with Microsoft/Challenge-project-foreach-if-array-CSharp-main/ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37fc509e3fbe5a83a2fa2001e414d35ded3ce4af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed9b9ab00f703e3810287e51c24c10ccc10bcbbf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
