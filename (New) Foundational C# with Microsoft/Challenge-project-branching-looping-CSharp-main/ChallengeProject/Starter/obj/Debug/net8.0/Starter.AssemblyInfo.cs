4ccf61427e7da1f2125b51a0cd9a93da07ad8785
Oageng Mashele <oageng.mashele@betterhome.co.za>
2024-08-20 15:52:12 +0200

    Challenge

--- (New) Foundational C# with Microsoft/Challenge-project-branching-looping-CSharp-main/ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ (New) Foundational C# with Microsoft/Challenge-project-branching-looping-CSharp-main/ChallengeProject/Starter/obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d772574da10d3b183bd1d9aad793cc7fcacd8a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7034531ed934fbde788d3c486adf164231724a22")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
