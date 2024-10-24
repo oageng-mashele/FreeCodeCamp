68a7edb9e758869b42a46a37759f47b2d1a472a3
Oageng Mashele <oageng.mashele@betterhome.co.za>
2024-10-24 15:35:54 +0200

    Exercise - Add a data store

--- (New) Foundational C# with Microsoft/ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ (New) Foundational C# with Microsoft/ContosoPizza/obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2514d966448dc2c10137261ad4da5046df26b14b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb8b3125549a7f3621f4861e1c47876aeec8754e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
