0ebe155c767c8a50d60308f3691c02c936b80638
Oageng Mashele <oageng.mashele@betterhome.co.za>
2024-10-24 09:26:26 +0200

    dotnetdependenciesupdates

--- (New) Foundational C# with Microsoft/DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
+++ (New) Foundational C# with Microsoft/DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd33c44654273fd00692b97bc50d290bc32ffc5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48df505cd34ec50d8a959304d913d7edf66b23cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
