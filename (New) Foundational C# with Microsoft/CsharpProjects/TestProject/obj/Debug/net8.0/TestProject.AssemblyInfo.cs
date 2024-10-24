2514d966448dc2c10137261ad4da5046df26b14b
Oageng Mashele <oageng.mashele@betterhome.co.za>
2024-10-24 15:16:47 +0200

    ASP.NET Core controllers

--- (New) Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ (New) Foundational C# with Microsoft/CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd33c44654273fd00692b97bc50d290bc32ffc5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3006b1999477bde3fdc78f3e20628f74a696f5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
